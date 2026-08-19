704f2d09ffb314a44925720341a99eb794c5161a
AlexeiNumberOne <alexeinumberone@yandex.ru>
2026-08-19 08:33:58 +0300

    Refactoring dwh and added clickhouse settings

--- dwh/dbms_utils.py
+++ dwh/dbms_utils.py
@@ -19,19 +19,39 @@ class PostgresSettings(BaseSettings):
     NAME: str
 
     @property
-    def sync_pg_url(self) -> str:
+    def sync_url(self) -> str:
         return f"postgresql+psycopg://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.NAME}"
 
     @property
-    def async_pg_url(self) -> str:
+    def async_url(self) -> str:
         return f"postgresql+asyncpg://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.NAME}"
 
     model_config = SettingsConfigDict(env_file=".env", env_prefix="PG_", extra="ignore")
 
 
-class PostgresManager:
-    def __init__(self, use_async: bool = False):
-        self.settings = PostgresSettings()
+class ClickHouseSettings(BaseSettings):
+    HOST: str
+    PORT: str
+    USER: str
+    PASS: str
+    NAME: str
+
+    @property
+    def sync_url(self) -> str:
+        return f"clickhouse+native://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.NAME}"
+
+    @property
+    def async_url(self) -> str:
+        return f"clickhouse+asynch://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env", env_prefix="CH_", extra="ignore")
+
+
+class DBMSManager:
+    def __init__(
+        self, settings: PostgresSettings | ClickHouseSettings, use_async: bool = False
+    ):
+        self.settings = settings
 
         self._sync_engine: Engine = None
         self._async_engine: AsyncEngine = None
@@ -39,15 +59,12 @@ class PostgresManager:
         self.metadata = MetaData()
         self.models: dict[str, Table] = {}
 
-        self.__use_async = use_async
-        if self.__use_async:
+        if use_async:
             if self._async_engine is None:
-                self._async_engine = create_async_engine(self.settings.async_pg_url)
-                self._async_engine
+                self._async_engine = create_async_engine(self.settings.async_url)
         else:
             if self._sync_engine is None:
-                self._sync_engine = create_engine(self.settings.sync_pg_url)
-                self._sync_engine
+                self._sync_engine = create_engine(self.settings.sync_url)
 
     def register_models(self, *args: TableFactory) -> None:
         """
@@ -60,14 +77,14 @@ class PostgresManager:
 
     def execute_sync(self, func, *args, **kwargs):
         if self._sync_engine is None:
-            raise ValueError(f"Экземпляр класса {PostgresManager.__name__} был создан в асинхронном режиме\
-                             \nДля асинхронного режима - method: execute_async, либо при создании экземпляра use_async=False")
+            raise ValueError(f"Экземпляр класса {DBMSManager.__name__} был создан в асинхронном режиме\
+                                \nДля асинхронного режима - method: execute_async, либо при создании экземпляра use_async=False")
         with self._sync_engine.connect() as conn:
             return func(conn, *args, **kwargs)
 
     async def execute_async(self, func, *args, **kwargs):
         if self._async_engine is None:
-            raise ValueError(f"Экземпляр класса {PostgresManager.__name__} был создан в синхронном режиме\
+            raise ValueError(f"Экземпляр класса {DBMSManager.__name__} был создан в синхронном режиме\
                              \nДля синхронного режима - method: execute_sync, либо при создании экземпляра use_async=True")
         async with self._async_engine.connect() as conn:
             return await conn.run_sync(func, *args, **kwargs)
